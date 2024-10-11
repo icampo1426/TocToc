67d9b21968ebd1e4e947463ea9289d3137c7b5a5
sriva832 <sriva@uade.edu.ar>
2024-10-11 10:16:11 -0300

    Update AddService.java

--- src/main/java/com/grupo02/toctoc/services/AddService.java
+++ src/main/java/com/grupo02/toctoc/services/AddService.java
@@ -1,7 +1,12 @@
 package com.grupo02.toctoc.services;
 
+import com.grupo02.toctoc.models.Add;
 import com.grupo02.toctoc.repository.AddRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AddService {
@@ -9,6 +14,21 @@ public class AddService {
 
     public AddService(AddRepository addRepository) {
         this.addRepository = addRepository;
+    }
+
+/ Obtener todos los anuncios
+    public List<Add> getAllAdds() {
+        return addRepository.findAll();
+    }
+
+    // Obtener un anuncio por su ID
+    public Optional<Add> getAddById(Long id) {
+        return addRepository.findById(id);
+    }
+
+    // Eliminar un anuncio por su ID
+    public void deleteAddById(Long id) {
+        addRepository.deleteById(id);
     }
 
     // Métodos de lógica empresarial
